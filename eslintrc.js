4872b675eb788c60acdc0734e7f683076ba0afc3
Jan Manikowski <janek210102j@gmail.com>
2025-07-07 02:33:40 +0200

    eslintrc

---
+++ eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['next', 'next/core-web-vitals', 'eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    '@next/next/no-img-element': 'off',
+  },
+};
