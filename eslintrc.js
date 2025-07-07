c5d15561e8f5a48ddca753b6e393e10488d740e3
Jan Manikowski <janek210102j@gmail.com>
2025-07-07 02:35:23 +0200

    changes

--- eslintrc.js
+++ eslintrc.js
@@ -1,5 +1,6 @@
+// .eslintrc.js
 module.exports = {
-  extends: ['next', 'next/core-web-vitals', 'eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['next', 'next/core-web-vitals'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
